595998f68bf56a0fdfca018d03ed4a0da50eb8c8
Zach <47896974+Zachdehooge@users.noreply.github.com>
2025-07-14 16:30:36 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ fn getos() {
 
 fn main() {
     let path = env::current_dir();
-
     // TODO: After getting the OS info, handle the case where Windows needs "\" and MacOS and Linux
     // needs "/"
 
     println!(
         "Path to the source file: {}\\src\\main.rs",
         path.expect("NO PATH FOUND").display()
-    )
+    );
+
+    println!("{}", env::consts::OS);
 }
